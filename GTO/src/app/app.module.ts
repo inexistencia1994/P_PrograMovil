08476a9c2f056ff053e4d1ee7c0f1b0cb7738fd9
marti_alcaino <63439337+inexistencia1994@users.noreply.github.com>
2024-10-24 18:03:20 -0300

    Implementación preliminar FireBase

--- GTO/src/app/app.module.ts
+++ GTO/src/app/app.module.ts
@@ -4,11 +4,20 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
+    AngularFireModule, AngularFirestoreModule, AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"bibliomovil-91326","appId":"1:400460088850:web:0e5b25e5838b4837a9bacb","storageBucket":"bibliomovil-91326.appspot.com","apiKey":"AIzaSyAJ3Kw8Tli5jmxFi_Nb7K4VMl21tGhYOVU","authDomain":"bibliomovil-91326.firebaseapp.com","messagingSenderId":"400460088850","measurementId":"G-4XZXVQL8TZ"})), provideAuth(() => getAuth()), provideStorage(() => getStorage())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- GTO/src/environments/environment.ts
+++ GTO/src/environments/environment.ts
@@ -1,11 +1,26 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export const environment = {
-  production: false
+  production: false,
+   
+  firebaseConfig : {
+    apiKey: "AIzaSyAJ3Kw8Tli5jmxFi_Nb7K4VMl21tGhYOVU",
+    authDomain: "bibliomovil-91326.firebaseapp.com",
+    projectId: "bibliomovil-91326",
+    storageBucket: "bibliomovil-91326.appspot.com",
+    messagingSenderId: "400460088850",
+    appId: "1:400460088850:web:0e5b25e5838b4837a9bacb",
+    measurementId: "G-4XZXVQL8TZ"
+  }
+  
 };
 
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
